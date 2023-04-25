4da8a6a0c3164f33fd0c29ee64c1a2ca77715a25
deskhit <131749111+deskhit@users.noreply.github.com>
2023-04-25 12:18:13 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const uri='';
+const uri='mongodb+srv://loan21:loan21@cluster0.i6x39wy.mongodb.net/test';
 const connecttomongo=()=>{
     mongoose.connect(uri).then((data)=>{
         console.log('Connected to databse successfully '+ data.Connection.name)
